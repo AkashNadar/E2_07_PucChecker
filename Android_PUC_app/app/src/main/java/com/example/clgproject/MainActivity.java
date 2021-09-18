a43540b5eea03a4ad2139b1188539d133a04735e
Akash <Akashnadar14@gmail.com>
2021-09-18 22:09:34 +0530

    BroadcastReceiver Added

--- Android_PUC_app/app/src/main/java/com/example/clgproject/MainActivity.java
+++ Android_PUC_app/app/src/main/java/com/example/clgproject/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.clgproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    MyReceiver myReceiver = new MyReceiver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +27,19 @@ public class MainActivity extends AppCompatActivity {
     public void UserSignUp(View view) {
         Intent SignUp = new Intent(MainActivity.this, ActivitySignUp.class);
         startActivity(SignUp);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        this.registerReceiver(myReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        this.unregisterReceiver(myReceiver);
     }
 }
